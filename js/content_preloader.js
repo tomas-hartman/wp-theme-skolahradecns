ecbf74b828a369c8a56b6aff2dd69dd5e08aa80e
tomas-hartman <tomas.hartman@hotmail.com>
2019-01-12 23:06:43 +0100

    first

---
+++ js/content_preloader.js
@@ -0,0 +1,4 @@
+/* 
+Cíl: chci, aby mi to načetlo iframe/obrázek předtím, než se k němu dostanu, ale zároveň, aby mi jeho loadování nezabíralo čas při načítání webu
+     hlavní cíl je zrychlit web.
+*/
