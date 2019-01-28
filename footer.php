ccf4dc244b89d652ecd14438e6a6d6b8308f76e0
tomas-hartman <tomas.hartman@hotmail.com>
2019-01-28 01:15:57 +0100

    flex box

--- footer.php
+++ footer.php
@@ -1,3 +1,48 @@
+<footer id="ft-flex">
+    <div class="horni">
+        <span class="ft-headline"><h3>Projekty a sponzoři</h3></span>
+        <div class="ft-flex-horni-cont">
+            <section class="sponsors"><a href=""></a><img src="<?php bloginfo('template_directory'); ?>/img/logolink.jpg" alt=""></section>
+            <section class="sponsors"><a href="http://www.etwinning.cz/"></a><img src="<?php bloginfo('template_directory'); ?>/img/etwinning.svg" alt=""></section>
+            <section class="sponsors"><a href="http://visegradfund.org/"></a><img src="<?php bloginfo('template_directory'); ?>/img/visegrad_fund_logo_blue_290px.png" alt=""></section>
+            <section class="sponsors"><a href="http://www.skutecnezdravaskola.cz/"></a><img src="<?php bloginfo('template_directory'); ?>/img/skutecnezdravaskola.jpg" alt=""></section>
+            <section class="sponsors"><a href="http://www.msmt.cz/"></a><img src="<?php bloginfo('template_directory'); ?>/img/msmtlogo.jpg" alt=""></section>
+            <section class="sponsors"><a href=""></a><p>Škola je příjemcem neinvestičních dotací "Podpora výuky plavání v základních školách v roce 2017 a Podpora zabezpečení mateřských škol a základních škol tvořených třídami 1. stupně s počtem tříd do pěti.</p></section>
+            <section class="sponsors"><a href="http://www.nadacecez.cz/"></a><img src="<?php bloginfo('template_directory'); ?>/img/logo_cez.png" alt=""></section>
+        </div>
+    </div>
+    <div class="stredni">
+        <div class="kategorie flex-cont">
+            <span class="ft-headline"><h3>Kategorie článků</h3></span>
+            <div class="list flex-item">
+                <?php 
+                    $args = array(
+                                'style' => 'none',
+                                'title_li' => ''
+                            );
+                    wp_list_categories( $args ); ?>
+            </div>  
+        </div>
+        <div class="archiv flex-cont">
+            <span class="ft-headline"><h3>Archiv článků</h3></span>
+            <ul class="flex-item">
+                <?php 
+                    $args = array(
+                                'type' => 'monthly',
+                                'limit' => '12',
+                                'format' => 'html',
+                                'order' => 'DESC'
+                            );
+                    wp_get_archives( $args );
+                ?>
+            </ul>
+        </div>
+    </div>
+    <div class="spodni">
+        <div class="left"><p>Copyright &copy; <?php echo date('Y'); ?> t3</p><p>Powered by WordPress</p></div>
+        <p class="right"><a href="<?php echo admin_url();?>">administrace</a></p>
+    </div>
+</footer>
 <footer id="ft-new">
     <div class="horni">
         <span class="ft-headline"><h3>Projekty a sponzoři</h3></span>
@@ -30,7 +75,12 @@
         </div>
     </div>
     <div class="spodni">
-
+        <p>Copyright &copy; <?php echo date('Y'); ?> t3</p><p>Powered by WordPress</p>
+        <p class="right"><a href="<?php echo admin_url();?>">administrace</a></p>
+    </div>
+    <div id="footer-spodni">
+        <p>Copyright &copy; <?php echo date('Y'); ?> t3</p><p>Powered by WordPress</p>
+        <p class="right"><a href="<?php echo admin_url();?>">administrace</a></p>
     </div>
 </footer>
 <footer>
