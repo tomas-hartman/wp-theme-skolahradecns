675f09b08aa2653fa174d26d83203282da4ff688
tomas-hartman <tomas.hartman@hotmail.com>
2020-03-18 22:58:52 +0100

    dev: menu finished, bugfixes needed

--- js/mainMenu.js
+++ js/mainMenu.js
@@ -1,6 +1,6 @@
 "use strict";
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   var menuItems = document.querySelectorAll("nav.main-menu .top-menu > li > a");
   var tappedElement;
 
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function() {
     var item = menuItems[i];
     item.parentNode.setAttribute("tabindex", "0");
 
-    item.addEventListener("click", function(e) {
+    item.addEventListener("click", function (e) {
       if (window.innerWidth > 960) return;
 
       if (!!tappedElement && tappedElement !== e.target.parentNode) {
@@ -19,10 +19,9 @@ document.addEventListener("DOMContentLoaded", function() {
       e.target.parentNode.classList.toggle("tap");
       e.target.nextElementSibling.addEventListener(
         "click",
-        function(e) {
+        function (e) {
           e.target.parentNode.classList.remove("tap");
-        },
-        {
+        }, {
           once: true
         }
       );
