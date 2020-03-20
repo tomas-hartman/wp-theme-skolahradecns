e27da1cc38461eaf53cda51285324443979ad009
tomas-hartman <tomas.hartman@hotmail.com>
2020-03-20 18:09:16 +0100

    fix: ie11 compatible js

--- js/mainMenu.js
+++ js/mainMenu.js
@@ -1,6 +1,10 @@
 "use strict";
 
+<<<<<<< HEAD
 document.addEventListener("DOMContentLoaded", function () {
+=======
+document.addEventListener("DOMContentLoaded", function() {
+>>>>>>> fix: ie11 compatible js
   var menuItems = document.querySelectorAll("nav.main-menu .top-menu > li > a");
   var tappedElement;
 
@@ -8,7 +12,11 @@ document.addEventListener("DOMContentLoaded", function () {
     var item = menuItems[i];
     item.parentNode.setAttribute("tabindex", "0");
 
+<<<<<<< HEAD
     item.addEventListener("click", function (e) {
+=======
+    item.addEventListener("click", function(e) {
+>>>>>>> fix: ie11 compatible js
       if (window.innerWidth > 960) return;
 
       if (!!tappedElement && tappedElement !== e.target.parentNode) {
@@ -19,12 +27,23 @@ document.addEventListener("DOMContentLoaded", function () {
       e.target.parentNode.classList.toggle("tap");
       e.target.nextElementSibling.addEventListener(
         "click",
+<<<<<<< HEAD
         function (e) {
           e.target.parentNode.classList.remove("tap");
         }, {
+=======
+        function(e) {
+          e.target.parentNode.classList.remove("tap");
+        },
+        {
+>>>>>>> fix: ie11 compatible js
           once: true
         }
       );
     });
   }
+<<<<<<< HEAD
 });
+=======
+});
+>>>>>>> fix: ie11 compatible js
